e37f2c34de431fde2f215f670e2bf1ecacab9ca3
Jason McDonald <jason.mcdonald@turtini.com>
2026-03-03 18:01:48 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,5 @@
+from turtini_sphinx_theme import get_theme_paths
+_paths = get_theme_paths()
+templates_path = [_paths["templates"]]
+html_static_path = [_paths["static"]]
+html_css_files = ["turtini.css"]
